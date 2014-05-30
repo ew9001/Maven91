d02b350b492614c64cbef0b0b78404c18c72a506
Earl Wilis <earl.willis@publicismodem.com>
2014-05-30 11:35:59 -0400

    Updated the structure

---
+++ PageLoads/src/main/java/test/nestle/CreateAccount.java
@@ -0,0 +1,241 @@
+package test.nestle;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class CreateAccount   
+{  
+  private static WebDriver driver;
+  public static final String USERNAME = "earlwillis1";
+  public String browser_type;
+	public static final String AUTOMATE_KEY = "XsPyFTirN4mH8aCLMB9A";
+	static String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+  @Test(groups = {"Create"})
+	@Parameters({"browser"})
+  @BeforeClass
+  public void beforeClass(String browser) throws IOException, InterruptedException
+  {	   
+	  
+	  
+	  if (browser.equals("firefox")) {
+		  driver=browserFirefox();
+		}
+	  
+
+	  else if (browser.equals("chrome")) {
+		  driver=browserChrome();
+		}
+	  
+	  else if (browser.equals("ie9")) {
+		  driver=browserIE9();
+		}
+	  
+
+	  else if (browser.equals("iPad")) {
+		  driver=browserIpad();
+		}
+	  
+	  else if (browser.equals("Android")) {
+		  driver=browserAndroid();
+		}
+	  
+	  
+	  
+	 
+		  System.out.println("Let me see which one get tested " +browser);
+
+		  
+	 
+	  System.out.println("Let me run get driver "+driver);
+	  
+		 
+		
+	
+	  System.out.println("Let me see which one get tested " +browser);
+       System.out.println("Let me run get driver "+driver);
+       driver.get("http://stage.coffee-mate.com/Registration/Create-Account.aspx?email=" + timeStamp +"%40yahoo.com&stt=True");
+       //driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtFirstName")).clear();
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtFirstName")).sendKeys("PubmoTestFirst");
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtLastName")).clear();
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtLastName")).sendKeys("PubmoTestLast");
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtPassword")).clear();
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtPassword")).sendKeys("Password123");
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtConfirmPassword")).clear();
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtConfirmPassword")).sendKeys("Password123");
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtAddress")).clear();
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtAddress")).sendKeys("1000 BroadwAY LANE");
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtAddress2")).clear();
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtAddress2")).sendKeys("Suite 3V");
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtCity")).sendKeys("New York");
+       new Select(driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_ddlStates"))).selectByVisibleText("Florida");
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtZipCode")).clear();
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_txtZipCode")).sendKeys("10003");
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_chkEmailCommunication")).click();
+      //  driver.findElement(By.name("flavorTheme")).click();
+       driver.findElement(By.id("ctl00_ContentPlaceHolder1_ucRegisterUser_btnRegister")).click();
+   	driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+   	
+   	   File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+   	   FileUtils.copyFile(scrFile, new File("Screenshots/" + timeStamp + "/" + "Successful-Registration-1.png"));
+   	   
+   	   
+   	   
+   	 new Select(driver.findElement(By.id("ctl00_ContentPlaceHolder1_lvQuestions_ctrl0_ucSurveyQuestion_ddlAnswers"))).selectByVisibleText("0");
+   	 driver.findElement(By.cssSelector("option[value=\"1E4C6A78-1980-459F-BE01-049AB4CB432C\"]")).click();
+   	 new Select(driver.findElement(By.id("ctl00_ContentPlaceHolder1_lvQuestions_ctrl1_ucSurveyQuestion_ddlAnswers"))).selectByVisibleText("0");
+   	 driver.findElement(By.cssSelector("option[value=\"138A6025-F98D-4A64-B36C-4A08DAB4F075\"]")).click();
+   	Thread.sleep(4000);
+   	 driver.findElement(By.id("ctl00_ContentPlaceHolder1_btnSubmit")).click();
+   	Thread.sleep(4000);
+   		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+       	Thread.sleep(4000);
+   		
+   		   File scrFile1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+   		   FileUtils.copyFile(scrFile1, new File("Screenshots/" + browser + "/" + timeStamp + "/" + "Successful-Registration-2.png"));
+   		  driver.quit();
+  }
+  @BeforeTest
+  public void beforeTest()
+  {
+	  System.out.println("Let me run beforeTest");
+  }  
+  @Test
+  public void f()
+  {
+       //your test code here
+	  System.out.println("I'm out here print f");
+  }
+ @AfterTest
+ public void afterTest()
+ {
+ }
+ @AfterClass
+ public void afterClass()
+ {
+     driver.quit();
+ }
+
+ public WebDriver browserFirefox() throws MalformedURLException  
+ {  
+  
+	 System.out.println("Let me run Firefox");
+	 String URL = "http://" + USERNAME + ":" + AUTOMATE_KEY + "@hub.browserstack.com/wd/hub";
+	 DesiredCapabilities caps = DesiredCapabilities.firefox();
+	  caps.setCapability("browser", "FireFox");
+	  caps.setCapability("browser_version", "22.0");
+	  caps.setCapability("os", "Windows");
+	  caps.setCapability("os_version", "7");
+	  caps.setCapability("browserstack.debug", "true");
+
+	    driver = new RemoteWebDriver(new URL(URL), caps);
+      System.out.println("Let me run Firefox");
+      browser_type="firefox";
+      System.out.println("Let me see which one get tested" +browser_type);
+      System.out.println("Let me see which one get tested" +driver);
+      driver.get("http://stage.coffee-mate.com/Registration/Create-Account.aspx?email=" + timeStamp +"%40yahoo.com&stt=True");
+      return driver;
+   
+    
+ }  
+
+ public WebDriver browserChrome() throws MalformedURLException  
+ {  
+  
+	 browser_type="chrome";
+	 String URL = "http://" + USERNAME + ":" + AUTOMATE_KEY + "@hub.browserstack.com/wd/hub";
+	 DesiredCapabilities caps = DesiredCapabilities.firefox();
+	 caps.setCapability("browser", "Chrome");
+	 caps.setCapability("browser_version", "32.0");
+	 caps.setCapability("os", "Windows");
+	 caps.setCapability("os_version", "7");
+	 caps.setCapability("resolution", "1024x768");
+
+	    driver = new RemoteWebDriver(new URL(URL), caps);
+      System.out.println("Let me run Chrome");
+      driver.get("http://stage.coffee-mate.com/Registration/Create-Account.aspx?email=" + timeStamp +"%40yahoo.com&stt=True");
+      
+      return driver;
+   
+    
+ }  
+ 
+ public WebDriver browserIE9() throws MalformedURLException  
+ {  
+  
+	
+	 String URL = "http://" + USERNAME + ":" + AUTOMATE_KEY + "@hub.browserstack.com/wd/hub";
+	 DesiredCapabilities caps = new DesiredCapabilities();
+	 caps.setCapability("browser", "IE");
+	 caps.setCapability("browser_version", "9.0");
+	 caps.setCapability("os", "Windows");
+	 caps.setCapability("os_version", "7");
+	 caps.setCapability("resolution", "1024x768");
+
+	    driver = new RemoteWebDriver(new URL(URL), caps);
+      System.out.println("Let me run IE9");
+      driver.get("http://stage.coffee-mate.com/Registration/Create-Account.aspx?email=" + timeStamp +"%40yahoo.com&stt=True");
+      
+      return driver;
+   
+    
+ }
+ 
+ public WebDriver browserIpad() throws MalformedURLException  
+ {  
+  
+
+	 String URL = "http://" + USERNAME + ":" + AUTOMATE_KEY + "@hub.browserstack.com/wd/hub";
+	 DesiredCapabilities caps = new DesiredCapabilities();
+	 caps.setCapability("browserName", "iPad");
+	 caps.setCapability("platform", "MAC");
+	 caps.setCapability("device", "iPad 3rd (7.0)");
+
+	    driver = new RemoteWebDriver(new URL(URL), caps);
+      System.out.println("Let me run Ipad");
+      driver.get("http://stage.coffee-mate.com/Registration/Create-Account.aspx?email=" + timeStamp +"%40yahoo.com&stt=True");
+      
+      return driver;
+   
+    
+ }
+ 
+ public WebDriver browserAndroid() throws MalformedURLException  
+ {  
+  
+	
+	 String URL = "http://" + USERNAME + ":" + AUTOMATE_KEY + "@hub.browserstack.com/wd/hub";
+	 DesiredCapabilities caps = new DesiredCapabilities();
+	 caps.setCapability("browserName", "android");
+	 caps.setCapability("platform", "ANDROID");
+	 caps.setCapability("device", "Samsung Galaxy Note 10.1");
+
+	    driver = new RemoteWebDriver(new URL(URL), caps);
+      System.out.println("Let me run Android");
+      driver.get("http://stage.coffee-mate.com/Registration/Create-Account.aspx?email=" + timeStamp +"%40yahoo.com&stt=True");
+      
+      return driver;
+   
+    
+ }
+
+}  
